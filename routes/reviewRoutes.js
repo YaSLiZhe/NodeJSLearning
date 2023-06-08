43dede40d2c09b20f7be66c62c671417ba029abe
LiZhe <1756368629@qq.com>
2023-06-07 23:36:08 -0400

    Add missing auth

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,11 +4,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -16,8 +16,14 @@ router
 
 router
   .route('/:id')
-  .delete(reviewController.deleteReview)
-  .patch(reviewController.updateReview)
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  )
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
   .get(reviewController.getReview);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,25 +8,19 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgetPassword', authController.forgetPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updatePassword',
-  authController.protect,
-  authController.updatePassword
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.get(
-  '/getMe',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
+//Protect all routes after this middleware
+router.use(authController.protect);
+router.patch('/updatePassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.get('/me', userController.getMe, userController.getUser);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
-
 router
   .route('/:id')
   .get(userController.getUser)
