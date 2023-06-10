fb70b90ab365d26a33af0a1ec24596360e6ba4f0
LiZhe <1756368629@qq.com>
2023-06-10 17:26:01 -0400

    Add preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,7 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   //populate to get associated data
@@ -87,7 +88,6 @@ reviewSchema.post('save', function () {
 // findByIdAndDelete
 reviewSchema.pre(/^findOneAnd/, async function (next) {
   this.r = await this.findOne();
-
   next();
 });
 
