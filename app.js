773240a656cc10777a50723333077d7f1fb156af
LiZhe <1756368629@qq.com>
2023-06-11 01:10:35 -0400

    Setting up the Project Structure

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -67,9 +68,8 @@ app.use((req, res, next) => {
 // Handling POST requests to create a new tour
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', { tour: 'ZHELI' });
-});
+app.use('/', viewRouter);
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
