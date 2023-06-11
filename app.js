dd9110c5f169dda8ca3da0239efb8e80e13a744c
LiZhe <1756368629@qq.com>
2023-06-11 00:14:17 -0400

    Add base.pug for fontend

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,9 +15,14 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //1) Gobal MIDDLEWARES
 // Set security HTTP header
 app.use(helmet());
+// Reaing data from a file and storing it in a variable
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Log HTTP requests
 if (process.env.NODE_ENV === 'development') {
@@ -52,9 +58,6 @@ app.use(
   })
 );
 
-// Reaing data from a file and storing it in a variable
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -64,6 +67,9 @@ app.use((req, res, next) => {
 // Handling POST requests to create a new tour
 
 //Routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', { tour: 'ZHELI' });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
