4c15fd70666b586e14b34d37f186c5846808177a
LiZhe <1756368629@qq.com>
2023-06-12 20:22:40 -0400

    Add rendering Error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -20,43 +20,69 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError('Your token is expired, please login it again', 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
-const sendErrorProd = (err, res) => {
-  //Operational Error We trusted:Send message to client
-  if (err.isOperational) {
+const sendErrorDev = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith('/api')) {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
+    });
+  } else {
+    //RENDERED
+    res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: err.message,
     });
   }
-  //Programming Error, don't leak detail to client
-  else {
+};
+
+const sendErrorProd = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith('/api')) {
+    //Operational Error We trusted:Send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong',
+        msg: err.message,
+      });
+    }
+    //Programming Error, don't leak detail to client
+
     //1} Log Error
     // console.error('Error', err);
     //2} Send Generic Error
-    res.status(500).json({
-      status: 'error',
+    return res.status(err.statusCode).render('error', {
+      title: 'Something wen wrong',
       message: 'Something went Wrong!',
     });
   }
+  //Rendered web
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: err.message,
+    });
+  }
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: 'Please try again later',
+  });
 };
-
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err, name: err.name, code: err.code };
+    let error = {
+      ...err,
+      name: err.name,
+      code: err.code,
+      message: err.message,
+    };
     //Bug error.name = undefined, solved by adding name: err.name, code: err.code
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
@@ -67,6 +93,6 @@ module.exports = (err, req, res, next) => {
       error = handleValidationErrorDB(error);
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
